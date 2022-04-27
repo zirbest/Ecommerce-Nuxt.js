303a96c73f9eca15a9ab07d51b3b3bcbb57809fe
zirbest <zirbst@gmail.com>
2022-04-27 22:25:13 +0100

    add: header

---
+++ components/Header.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+const isMenu = ref(false);
+const isSearch = ref(false);
+</script>
+
+<template>
+  <header
+    class="flex items-center justify-between b flex-wrap sm:flex-nowrap"
+    p="x4 y2"
+  >
+    <a href="#" class="text-black no-underline">
+      <div class="flex items-center gap-2">
+        <span class="inline-block w-10 stroke-width-3"> <Logo /> </span>
+        <span class="text-sm font-semibold">E-Shop</span>
+      </div>
+    </a>
+
+    <div
+      v-if="isMenu"
+      class="b flex flex-col gap-10 w-full mt-4 order-2"
+      sm="mt-auto flex-row w-auto gap-14 order-none"
+    >
+      <a hover="op70 bg-gray-2" font-500 text-center sm="text-auto" href="#"
+        >Men</a
+      >
+      <a hover="op70 bg-gray-2" font-500 text-center sm="text-auto" href="#"
+        >Women</a
+      >
+      <a hover="op70 bg-gray-2" font-500 text-center sm="text-auto" href="#"
+        >Kids</a
+      >
+    </div>
+
+    <div class="space-x-2">
+      <button
+        class="w-6 h-6 p-0 bg-transparent cursor-pointer b-0"
+        @click="isSearch = !isSearch"
+      >
+        <span class="w-full h-full i-carbon:search" />
+      </button>
+      <button class="w-6 h-6 p-0 bg-transparent cursor-pointer b-0">
+        <span class="w-full h-full i-carbon:shopping-cart" />
+      </button>
+      <button
+        sm="hidden"
+        class="w-6 h-6 p-0 bg-transparent cursor-pointer b-0"
+        @click="isMenu = !isMenu"
+      >
+        <span class="w-full h-full i-carbon:menu" />
+      </button>
+    </div>
+  </header>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,8 +18,19 @@ export default defineNuxtConfig({
     uno: true,
     attributify: true,
     preflight: true,
+    theme: {
+      fontFamily: {
+        sans: '"Inter", Inter var,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
+      },
+    },
     icons: {
       scale: 1.2,
+      extraProperties: {
+        'display': 'inline-block',
+        'height': '1.2em',
+        'width': '1.2em',
+        'vertical-align': 'text-bottom',
+      },
     },
     shortcuts: [
       ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
