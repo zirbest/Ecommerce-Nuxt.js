5cde53ef7502f53d68e2f2bfc9a715c3f6af3b2e
zirbest <zirbst@gmail.com>
2022-05-09 23:33:32 +0100

    add: hero

---
+++ components/Hero.vue
@@ -0,0 +1,13 @@
+<template>
+  <div flex-1 flex items-center>
+    <div ml20>
+      <h2 fw-bold text-15 c-white mb-8 max-w15ch>Make your House into a Home</h2>
+      <button flex gap-2 c-white items-center>
+        <span bg-blue6 rounded-full p-2 w-10 h-10>
+          <span i-carbon-arrow-right></span>
+        </span>
+        <span fw-600>SHOP NOW</span>
+      </button>
+    </div>
+  </div>
+</template>
