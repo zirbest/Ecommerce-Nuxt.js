e9d3b04981491d7bb91f44c1fcdc3ec9fc3b9fdc
zirbest <zirbst@gmail.com>
2022-05-13 11:47:57 +0100

    chore: use unocss shortcut button

--- components/selected-for-you.vue
+++ components/selected-for-you.vue
@@ -1,19 +1,10 @@
 <template>
   <div container mxa mt20>
     <div flex justify-between items-center>
-      <p text-xl>Selected just for you</p>
-      <a
-        ws-nowrap
-        hover:o780
-        b
-        b-gray
-        text="slate-8 xs"
-        font-bold
-        p="x3 y2"
-        rounded-full
-      >
-        SHOW MORE
-      </a>
+      <!-- <p text-xl>Selected just for you</p> -->
+      <!-- <a btn> SHOW MORE </a> -->
+      <!-- <p text-xl w-full text-center>Selected just for you</p> -->
+      <slot />
     </div>
     <ProductCard />
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
       },
     },
     shortcuts: [
-      ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+      ['btn', 'px-4 py-1 rounded-full inline-block text-slate-8 b b-gray cursor-pointer hover:op-80 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ],
   },
   colorMode: {
