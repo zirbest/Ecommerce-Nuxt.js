1b01a09273ef6f84219fc4484a9d6f405ea0873a
zirbest <zirbst@gmail.com>
2022-05-13 11:17:07 +0100

    refactor: use <UnoIcon/>

--- components/header-why-chose-us.vue
+++ components/header-why-chose-us.vue
@@ -1,6 +1,6 @@
 <template>
   <div grid="~ cols-[repeat(auto-fit,minmax(14rem,1fr))]">
-    <div
+    <ul
       p4
       bg-white
       grid="~ cols-[max-content]"
@@ -9,18 +9,18 @@
       v-for="i in 3"
     >
       <span row-span-2 h-14 w-14 p2 rounded-lg bg-yellow:20>
-        <span h-full w-full block i-carbon:delivery bg-yellow6></span>
+        <UnoIcon h-full w-full block i-carbon:currency bg-yellow6 />
       </span>
       <span font-semibold c-slate-9>Free Shiping</span>
-      <span col-start-2 text-sm c-slate-5>On purchases over 999</span>
-    </div>
+      <span col-start-2 text-sm c-slate-5>Dolor quos reiciendis vitae a quis Error quae.</span>
+    </ul>
 
     <div order="first lg:none" basis-50 flex gap-x4 items-center justify-center>
       <div bg-white:15>
-        <span p3 m2 c-white i-carbon-arrow-left></span>
+        <UnoIcon p3 m2 c-white i-carbon-arrow-left />
       </div>
       <div bg-white>
-        <span p3 m2 i-carbon-arrow-right></span>
+        <UnoIcon p3 m2 i-carbon-arrow-right />
       </div>
     </div>
   </div>
