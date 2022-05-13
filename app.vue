92625623fcffb05cd6b4ec0ab946a48ce5dd23ff
zirbest <zirbst@gmail.com>
2022-05-13 11:19:32 +0100

    chore: update package and MD files

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <script setup>
 useHead({
-  title: 'Vitesse Nuxt 3',
+  title: 'Ecomerce Web App Using Nuxt',
   link: [
     {
       rel: 'icon', type: 'image/png', href: '/nuxt.png',
---
+++ components/why-should-choose-us.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mxa container">
+    <h3 class="text-center my30 text-2xl">Why Should Choose us?</h3>
+    <ul flex="~ wrap" class="max-w[max-content] mxa">
+      <li flex="~ col" class="p4" >
+        <span mb3 inline-block h-14 w-14 p2 rounded-lg bg-gray:20>
+          <UnoIcon h-full w-full block i-carbon:delivery bg-gray6 />
+        </span>
+        <h4 mb1 fw-semibold>Free Shiping</h4>
+        <p class="max-w25ch c-slate4" text-sm>Dolor quos reiciendis vitae a quis Error quae.</p>
+      </li>
+
+      <li flex="~ col" class="p4" >
+        <span mb3 inline-block h-14 w-14 p2 rounded-lg bg-yellow:20>
+          <UnoIcon h-full w-full block i-carbon:currency bg-yellow5 />
+        </span>
+        <h4 mb1 fw-semibold>Easy Payments</h4>
+        <p class="max-w25ch c-slate4" text-sm>Dolor quos reiciendis vitae a quis Error quae.</p>
+      </li>
+
+      <li flex="~ col" class="p4" >
+        <span mb3 inline-block h-14 w-14 p2 rounded-lg bg-gray:20>
+          <UnoIcon h-full w-full block i-carbon:ibm-security bg-gray6 />
+        </span>
+        <h4 mb1 fw-semibold>Money-Back Guarantee</h4>
+        <p class="max-w25ch c-slate4" text-sm>Dolor quos reiciendis vitae a quis Error quae.</p>
+      </li>
+
+      <li flex="~ col" class="p4" >
+        <span mb3 inline-block h-14 w-14 p2 rounded-lg bg-gray:20>
+          <UnoIcon h-full w-full block i-mdi:blur bg-gray6 />
+        </span>
+        <h4 mb1 fw-semibold>Finest Quality</h4>
+        <p class="max-w25ch c-slate4" text-sm>Dolor quos reiciendis vitae a quis Error quae.</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
